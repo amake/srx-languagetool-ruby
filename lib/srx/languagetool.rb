9609ec6ebd840600b349cb47ddd14c5839dd7a4c
Aaron Madlon-Kay <aaron@madlon-kay.com>
2021-02-13 21:41:50 +0900

    bundle gem srx-languagetool

---
+++ lib/srx/languagetool.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "languagetool/version"
+
+module Srx
+  module Languagetool
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/srx/languagetool/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Srx
+  module Languagetool
+    VERSION = "0.1.0"
+  end
+end
